5f747a2660772980944572137a438bb36b0fade3
Boris <boriskoznev@gmail.com>
2024-06-15 18:15:53 +0300

    removed button animation in reg, changed button width, reset css, add icons near inputs

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -11,32 +11,38 @@ function Register() {
     className: "reg-page"
   }, /*#__PURE__*/_react.default.createElement("h1", null, /*#__PURE__*/_react.default.createElement("span", {
     className: "accent-color"
-  }, "Miku"), " Notes"), /*#__PURE__*/_react.default.createElement("form", {
+  }, "Miku"), " Notes"), /*#__PURE__*/_react.default.createElement("p", null, "Your notes and to-do lists, always at hand.", /*#__PURE__*/_react.default.createElement("br", null), "Simple, fast, ", /*#__PURE__*/_react.default.createElement("span", {
+    className: "accent-color"
+  }, "clean.")), /*#__PURE__*/_react.default.createElement("form", {
     action: ""
   }, /*#__PURE__*/_react.default.createElement("div", {
     className: "reg-form-container"
-  }, /*#__PURE__*/_react.default.createElement("input", {
+  }, /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("i", {
+    className: "fa-solid fa-user"
+  }), /*#__PURE__*/_react.default.createElement("input", {
     type: "email",
     placeholder: "Email",
     name: "email",
     id: "email",
     required: true
+  })), /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("i", {
+    className: "fa-solid fa-lock"
   }), /*#__PURE__*/_react.default.createElement("input", {
     type: "password",
     placeholder: "Enter Password",
     name: "psw",
     id: "psw",
     required: true
+  })), /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("i", {
+    className: "fa-solid fa-lock"
   }), /*#__PURE__*/_react.default.createElement("input", {
     type: "password",
     placeholder: "Repeat Password",
     name: "psw-repeat",
     id: "psw-repeat",
     required: true
-  }), /*#__PURE__*/_react.default.createElement("button", {
+  })), /*#__PURE__*/_react.default.createElement("button", {
     type: "submit",
     className: "registerbtn"
-  }, "Sign up ", /*#__PURE__*/_react.default.createElement("i", {
-    className: "fa-solid fa-arrow-right"
-  })))));
+  }, "Sign up"))));
 }
--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -6,15 +6,23 @@ export default function Register() {
             <h1>
                 <span className='accent-color'>Miku</span> Notes
             </h1>
+            <p >Your notes and to-do lists, always at hand.<br></br>
+                Simple, fast, <span className='accent-color'>clean.</span></p>
             <form action="">
                 <div className="reg-form-container">
-                    <input type="email" placeholder="Email" name='email' id="email" required />
-
-                    <input type="password" placeholder="Enter Password" name="psw" id="psw" required />
-
-                    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required />
-                    <button type="submit" className="registerbtn">Sign up <i className="fa-solid fa-arrow-right"></i>
-                    </button>
+                    <div>
+                        <i className="fa-solid fa-user"></i>
+                        <input type="email" placeholder="Email" name='email' id="email" required />
+                    </div>
+                    <div>
+                        <i className="fa-solid fa-lock"></i>
+                        <input type="password" placeholder="Enter Password" name="psw" id="psw" required />
+                    </div>
+                    <div>
+                        <i className="fa-solid fa-lock"></i>
+                        <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required />
+                    </div>
+                    <button type="submit" className="registerbtn">Sign up</button>
                 </div>
             </form>
         </section>
